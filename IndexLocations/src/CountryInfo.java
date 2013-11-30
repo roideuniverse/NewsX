99b67071bb4f2477fd8f9833a64b67e211a66d6e
roideuniverse <roideuniverse@gmail.com>
2013-11-30 18:22:33 -0500

    removeing some comments

--- IndexLocations/src/CountryInfo.java
+++ IndexLocations/src/CountryInfo.java
@@ -35,44 +35,14 @@ public class CountryInfo {
 			// Get the object of DataInputStream
 			DataInputStream in = new DataInputStream(fstream);
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			//String prevLine="";
+
 			String line ;
-			
-			/*while((line=br.readLine()) != null) {
-				if(!line.startsWith("#"))
-					break;
-				//prevLine = line;
-			}*/
-			//String headers[] = prevLine.split("\t");
-			//for(int i=0;i<headers.length; i++) {
-				//System.out.print("" + headers[i] + "\t");
-			//}
-			//System.out.println("\n");
 			while((line=br.readLine()) != null ) {
 				if(line.startsWith("#"))
 					continue;
 				//System.out.println("" + line);
 				String[] values = line.split("\t");	
-				//System.out.println("XX:" + values[0]);
-				//System.out.println("" + values[1]);
-				//System.out.println("" + values[2]);
-				//System.out.println("" + values[3]);
-				//System.out.println("Country:" + values[4]);
 				mapCountryCode.put(values[0].trim(), values[4].trim());
-				//System.out.println("capital:" + values[5]);
-				//System.out.println("" + values[6]);
-				//System.out.println("" + values[7]);
-				//System.out.println("continent:" + values[8]);
-				//System.out.println("" + values[9]);
-				//System.out.println("" + values[10]);
-				//System.out.println("" + values[11]);
-				//System.out.println("" + values[12]);
-				//System.out.println("" + values[13]);
-				//System.out.println("" + values[14]);
-				//System.out.println("" + values[15]);
-				//System.out.println("" + values[16]);
-				//System.out.println("" + values[17]);
-				//System.out.println("" + values[18]);
 			}
 			// Close the input stream
 			in.close();
