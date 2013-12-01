50ed471d918e4452d0c016f9341a2c947f1390ee
roideuniverse <roideuniverse@gmail.com>
2013-11-30 20:08:00 -0500

    Added error if the server url is not given

--- IndexLocations/src/ir/search/IndexDoc.java
+++ IndexLocations/src/ir/search/IndexDoc.java
@@ -15,33 +15,36 @@ import org.apache.solr.client.solrj.impl.XMLResponseParser;
 import org.apache.solr.core.CoreContainer;
 import org.xml.sax.SAXException;
 
-
 public class IndexDoc {
-	
-	public void implementIndexing(List<WikipediaDocument> doc) throws SAXException,ParserConfigurationException,IOException,SolrServerException
-	{
-		try{
-				System.out.println("Index Started");
-		
-				  HttpSolrServer server = new HttpSolrServer(FileUtil.getUrl());
-				  server.setMaxRetries(1); // defaults to 0.  > 1 not recommended.
-				  server.setConnectionTimeout(5000); // 5 seconds to establish TCP
-				  
-				  server.setParser(new XMLResponseParser()); // binary parser is used by default
-				 
-				  server.setSoTimeout(1000);  // socket read timeout
-				  server.setDefaultMaxConnectionsPerHost(100);
-				  server.setMaxTotalConnections(100);
-				  server.setFollowRedirects(false);  // defaults to false
-				 
-				  server.setAllowCompression(true);
-				  server.addBeans(doc);
-				  server.commit();
-		}
-		catch(Exception e)
-		{
-			
-			System.out.println("Message:"+e.getMessage());
+
+	public void implementIndexing(List<WikipediaDocument> doc)
+			throws SAXException, ParserConfigurationException, IOException,
+			SolrServerException {
+		try {
+			System.out.println("Index Started");
+			String serverUrl = FileUtil.getUrl();
+			if( serverUrl == null || serverUrl.isEmpty()) {
+				System.err.println("Server url empty");
+				return;
+			}
+			HttpSolrServer server = new HttpSolrServer(FileUtil.getUrl());
+			server.setMaxRetries(1); // defaults to 0. > 1 not recommended.
+			server.setConnectionTimeout(5000); // 5 seconds to establish TCP
+
+			server.setParser(new XMLResponseParser()); // binary parser is used
+														// by default
+
+			server.setSoTimeout(1000); // socket read timeout
+			server.setDefaultMaxConnectionsPerHost(100);
+			server.setMaxTotalConnections(100);
+			server.setFollowRedirects(false); // defaults to false
+
+			server.setAllowCompression(true);
+			server.addBeans(doc);
+			server.commit();
+		} catch (Exception e) {
+			//e.printStackTrace();
+			System.out.println("Message:" + e.getMessage());
 		}
 	}
 
